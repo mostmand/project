b618b72f87c52c2dd00c33aca4b29b1f2d45fd62
akhavan <rasoul.am1376@gmail.com>
2016-04-17 23:12:45 +0430

    Enemy Class created. Some methods added to Enemy and Castle. abstractness is temporary.

--- TowerDefense/src/Castle.java
+++ TowerDefense/src/Castle.java
@@ -1,11 +1,12 @@
 /**
  * Created by akhavan on 2016-04-16.
  */
-public class Castle {
+public abstract class Castle {
     private int health;
     private int price;
     private int power;
     private int viewRange;
     private int attackspeed;
-    
+    public abstract void monitorSurroundings();
+    public abstract void attack();
 }
---
+++ TowerDefense/src/Enemy.java
@@ -0,0 +1,11 @@
+/**
+ * Created by akhavan on 2016-04-17.
+ */
+public abstract class Enemy {
+    private int health;
+    private int prize;
+    private int power;
+    private int attackspeed;
+    private int speed;
+    public abstract void monitorSurroundings();
+}
