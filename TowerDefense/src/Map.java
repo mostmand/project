c796f3b079efa6e19c337e2829522313f3eca779
Qasem <qniksefat@gmail.com>
2016-04-18 16:56:20 +0430

    Create the map

---
+++ TowerDefense/src/Map.java
@@ -0,0 +1,7 @@
+/**
+ * Created by qasem on 4/18/16.
+ */
+public class Map {
+    Square[] squares;
+    int length;
+}
---
+++ TowerDefense/src/User.java
@@ -0,0 +1,8 @@
+/**
+ * Created by qasem on 4/18/16.
+ */
+public class User {
+    int wealth;
+    Castle[] castles;
+
+}
