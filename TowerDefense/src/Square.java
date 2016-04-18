54cdce720ca54f1ecf90e6b63d7cf7bf95985a4b
MH Mostmand <mostmand.mh@gmail.com>
2016-04-18 13:51:53 +0430

    Created Square class

---
+++ TowerDefense/src/Square.java
@@ -0,0 +1,9 @@
+/**
+ * Created by mostm on 18/04/2016.
+ */
+public abstract class Square {
+    Integer xCoordination;
+    Integer yCoortdination;
+
+    public abstract void abstractOperation();
+}
