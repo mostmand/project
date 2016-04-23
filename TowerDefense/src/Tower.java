0a0ee2a160486d090ffe3e625e202e83623911e4
akhavan <rasoul.am1376@gmail.com>
2016-04-23 14:22:59 +0430

    Tower Finished

--- TowerDefense/src/Tower.java
+++ TowerDefense/src/Tower.java
@@ -6,14 +6,21 @@ import java.util.Timer;
 public abstract class Tower extends Military{
     Timer reloadtime = new Timer();
 
+    private Integer price;
     private Integer viewRange;
     private Integer power;
     private Integer attackspeed;
 
+    public Integer getPrice() {
+        return price;
+    }
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
     public Integer getViewRange() {
         return viewRange;
     }
-
     public void setViewRange(Integer viewRange) {
         this.viewRange = viewRange;
     }
@@ -21,7 +28,6 @@ public abstract class Tower extends Military{
     public Integer getPower() {
         return power;
     }
-
     public void setPower(Integer power) {
         this.power = power;
     }
@@ -29,65 +35,27 @@ public abstract class Tower extends Military{
     public Integer getAttackspeed() {
         return attackspeed;
     }
-
     public void setAttackspeed(Integer attackspeed) {
         this.attackspeed = attackspeed;
     }
 
-//        public abstract void monitorSurroundings();
-//    public abstract void attack();
-//    Sector[] surroundings;
-
-    @Override
-    public void monitorSurroundings(){
-        for (int radius = 1; radius <= viewRange; radius++){
-            for (int xdif = -radius; xdif <= radius; xdif++){
-                int ydif = radius - Math.abs(xdif);
-                if (this.neighboringSector(xdif, ydif) != null){
-                    if (this.neighboringSector(xdif, ydif).isOccupiedByEnemy()){
-                        attack(this.neighboringSector(xdif, ydif));
-                    }
-                }
-                ydif = -ydif;
-                if (this.neighboringSector(xdif, ydif) != null){
-                    if (this.neighboringSector(xdif, ydif).isOccupiedByEnemy()){
-                        attack(this.neighboringSector(xdif, ydif));
-                    }
-                }
-            }
-        }
-    }
-
-    @Override
-    public void attack(Map.Sector wheretoattack){
-        Enemy prey = null;
-        for (Military candidate:wheretoattack.occupant) {
-            if (prey == null || prey.getHealth() > candidate.getHealth())
-                prey = (Enemy) candidate;
-        }
-        if (prey != null)
-            prey.setHealth(prey.getHealth()-this.getPower());
+    public void hit(Object enemy) throws Exception{
+        if (!(enemy instanceof Enemy))
+            throw new Exception("Prey not an Enemy");
+        ((Enemy)enemy).setHealth(((Enemy)enemy).getHealth() - this.getPower());
     }
 
 }
 
 class Tower1 extends Tower{
 
-    Tower1(Map gameMap, Map.Sector sector) {
-        this.setGameMap(gameMap);
-        this.setPosition(sector);
-    }
-
     static Integer price = 11;
-    static Integer initialhealth = 1000;
-
     static Integer viewRange = 7;
     static Integer power = 200;
     static Integer attackSpeed = 300;
 
     Tower1(){
         this.setPrice(price);
-        this.setHealth(initialhealth);
         this.setViewRange(viewRange);
         this.setPower(power);
         this.setAttackspeed(attackSpeed);
