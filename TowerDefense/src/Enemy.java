9c25601a6c6f4868fa19c676e1453f121ffc16ee
MH Mostmand <mostmand.mh@gmail.com>
2016-04-23 14:05:10 +0430

    Military class redesigned

--- TowerDefense/src/Enemy.java
+++ TowerDefense/src/Enemy.java
@@ -7,16 +7,8 @@ public abstract class Enemy extends Military{
     Timer reloadtime = new Timer();
 
     private Integer price;
-    private Integer power;
     private Integer speed;
     private Integer health;
-
-    public void setPower(Integer power) {
-        this.power = power;
-    }
-    public Integer getPower() {
-        return power;
-    }
 
     public void setSpeed(Integer speed) {
         this.speed = speed;
@@ -25,36 +17,16 @@ public abstract class Enemy extends Military{
         return speed;
     }
 
-    @Override
-    public void monitorSurroundings(){
-        if(this.getGameMap().sectors[this.getSector().xCoordinate][this.getSector().yCoordinate].isOccupiedByTower()){
-            attack();
-        }
-    }
-
-    @Override
-    public void attack() {
-
-    }
-
 }
 
 class Enemy1 extends Enemy{
     //the values here must be changed at a later time
-    static Integer price = 1;
+    static Integer cost = 1;
     static Integer speed = 300;
-    static Integer power = 200;
-    static Integer initialHealth = 15;
-
-
-    Enemy1(Map gameMap, Map.Sector sector) {
-        this.setGameMap(gameMap);
-        this.setSector(sector);
-    }
+    static Integer initialHealth = 200;
 
     Enemy1(){
-        this.setPrice(price);
-        this.setPower(power);
+        this.setPrice(cost);
         this.setSpeed(speed);
         this.setHealth(initialHealth);
     }
