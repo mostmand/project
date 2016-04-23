e7507cf2c0a4c329bf71d1d28fd970093ef4fdbd
akhavan <rasoul.am1376@gmail.com>
2016-04-23 14:06:39 +0430

    New Military

--- TowerDefense/src/Military.java
+++ TowerDefense/src/Military.java
@@ -3,73 +3,54 @@
  */
 public abstract class Military{
 
-    MilitaryThread myThread = new MilitaryThread(this);
+//    MilitaryThread myThread = new MilitaryThread(this);
 
 
-    private Integer health;
+//    private Integer health;
+//
+//    public Integer getHealth() {
+//        return health;
+//    }
+//    public void setHealth(Integer health) {
+//        this.health = health;
+//    }
+//
+//
+//    private Integer price;
+//
+//    public Integer getPrice() {
+//        return price;
+//    }
+//    public void setPrice(int price) {
+//        this.price = price;
+//    }
+//
+//
+//    private Map gameMap;
+//
+//    public Map getGameMap() {
+//        return gameMap;
+//    }
+//    public void setGameMap(Map gameMap) {
+//        this.gameMap = gameMap;
+//    }
+//
+//    private Map.Sector position;
+//
+//    public Map.Sector getPosition() {
+//        return position;
+//    }
+//    public void setPosition(Map.Sector sector) {
+//        this.position = sector;
+//    }
+//
+//    public Map.Sector neighboringSector(int xdif, int ydif){
+//        int newX = this.getPosition().xCoordinate + xdif;
+//        int newY = this.getPosition().yCoordinate + ydif;
+//        if (0 < newX && newX <= getGameMap().length && 0 < newY && newY <= getGameMap().length){
+//            return getGameMap().sectors[newX][newY];
+//        }
+//        return null;
+//    }
 
-    public Integer getHealth() {
-        return health;
-    }
-    public void setHealth(Integer health) {
-        this.health = health;
-    }
-
-
-    private Integer price;
-
-    public Integer getPrice() {
-        return price;
-    }
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-
-    private Map gameMap;
-
-    public Map getGameMap() {
-        return gameMap;
-    }
-    public void setGameMap(Map gameMap) {
-        this.gameMap = gameMap;
-    }
-
-    private Map.Sector position;
-
-    public Map.Sector getPosition() {
-        return position;
-    }
-    public void setPosition(Map.Sector sector) {
-        this.position = sector;
-    }
-
-    public Map.Sector neighboringSector(int xdif, int ydif){
-        int newX = this.getPosition().xCoordinate + xdif;
-        int newY = this.getPosition().yCoordinate + ydif;
-        if (0 < newX && newX <= getGameMap().length && 0 < newY && newY <= getGameMap().length){
-            return getGameMap().sectors[newX][newY];
-        }
-        return null;
-    }
-
-    public abstract void monitorSurroundings();
-
-    public abstract void attack(Map.Sector wheretoattack);
-
-}
-
-
-class MilitaryThread extends Thread{
-
-    MilitaryThread(Military militia) {
-        this.militia = militia;
-    }
-
-    Military militia;
-
-    @Override
-    public void run() {
-        militia.monitorSurroundings();
-    }
 }
