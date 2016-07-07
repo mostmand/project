1c682a82996c8c21f92564cacc366b2c5ce72b04
akhavan <rasoul.am1376@gmail.com>
2016-07-07 10:31:21 +0430

    Added methods to MilitaryType

--- TowerDefense/src/Logic/MilitaryForces/MilitaryType.java
+++ TowerDefense/src/Logic/MilitaryForces/MilitaryType.java
@@ -1,18 +1,47 @@
 package Logic.MilitaryForces;
 
+import com.sun.istack.internal.NotNull;
+
+import java.lang.reflect.Field;
+
 /**
  * Created by akhavan on 2016-06-09.
  */
 public enum MilitaryType {
-    BASIC(11),
-    FIRE(13),
-    TREE(14),
-    LIGHT(16),
-    DARK(15);
+    BASIC,
+    FIRE,
+    TREE,
+    LIGHT,
+    DARK;
 
-    public int initialPrice;
-
-    MilitaryType(int price){
-        this.initialPrice = price;
+    @NotNull
+    public Class<?> getTowerType(){
+        switch (this){
+            case BASIC:
+                return BasicTower.class;
+            case FIRE:
+                return FireTower.class;
+            case TREE:
+                return TreeTower.class;
+            case LIGHT:
+                return LightTower.class;
+            case DARK:
+                return DarkTower.class;
+        }
+        return null;
     }
+
+    @NotNull
+    public Integer initialPrice(){
+        try {
+            Field f = this.getTowerType().getDeclaredField("INITIAL_PRICE");
+            return (Integer) f.get(this.getClass());
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
 }
